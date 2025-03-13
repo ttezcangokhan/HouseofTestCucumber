bbe3103f40072930495c81cbef8d2f2086c211ad
ttezcangokhan <ttezcangokhan@gmail.com>
2025-03-13 21:06:36 +0100

    update2

--- src/main/java/pages/AllPages.java
+++ src/main/java/pages/AllPages.java
@@ -8,8 +8,8 @@ public class AllPages{
 
     }
 
-    //javada her class return type olabilir
-    //Her page class return type ile private instance olusturduk
+    //In Java, every class can have a return type.
+    //We created a private instance with each page class return type.
 
     public SeleniumBase lib=new SeleniumBase();
     private HouseOfTestHomePage houseOfTestHomePage;
@@ -17,7 +17,7 @@ public class AllPages{
     private HouseOfTestBlogsPage houseOfTestBlogsPage;
     private HouseOfTestContactUsPage houseOfTestContactUsPage;
 
-    //her page class icin public method olusturup obje olusturacagiz
+    //We will create a public method and create an object in each page class.
 
     public HouseOfTestHomePage houseOfTestHomePage(){
         if (houseOfTestHomePage ==null){
